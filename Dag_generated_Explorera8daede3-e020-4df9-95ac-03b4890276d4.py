6d13740bff49785c19833ef73b81cdf5721b309f
krishnadhoundiyal <46423210+krishnadhoundiyal@users.noreply.github.com>
2021-11-12 00:21:28 +0530

    Pushed DAG Dag_generated_Explorera8daede3-e020-4df9-95ac-03b4890276d4

---
+++ Dag_generated_Explorera8daede3-e020-4df9-95ac-03b4890276d4.py
@@ -0,0 +1,104 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow import models
+import os
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+from airflow.kubernetes.secret import Secret
+
+
+args = {
+    "project_id": "Dag_generated_Explorera8daede3-e020-4df9-95ac-03b4890276d4",
+}
+
+dag = DAG(
+    "Dag_generated_Explorera8daede3-e020-4df9-95ac-03b4890276d4",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="Take This from User",
+    is_paused_upon_creation=False,
+)
+
+
+# Ensure that the secret named '0PKhq1+tiY89IzXwwhF5MqXTYefFCfZl8mksiqe8' is defined in the Kubernetes namespace where this pipeline will be run
+env_var_secret_id = Secret(
+    deploy_type="env",
+    deploy_target="AWS_ACCESS_KEY_ID",
+    secret="0PKhq1+tiY89IzXwwhF5MqXTYefFCfZl8mksiqe8",
+    key="AWS_ACCESS_KEY_ID",
+)
+env_var_secret_key = Secret(
+    deploy_type="env",
+    deploy_target="AWS_SECRET_ACCESS_KEY",
+    secret="0PKhq1+tiY89IzXwwhF5MqXTYefFCfZl8mksiqe8",
+    key="AWS_SECRET_ACCESS_KEY",
+)
+
+
+op_e326c245cc8e4e1d8f3b059f548a1d07 = KubernetesPodOperator(
+    task_id="e326c245cc8e4e1d8f3b059f548a1d07",
+    name="e326c245cc8e4e1d8f3b059f548a1d07",
+    namespace="default",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/krishnadhoundiyal/explorer/main/runnotebook.py --output runnotebook.py && curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/krishnadhoundiyal/explorer/main/requirements-explorer.txt --output requirements.txt && python3 -m pip install packaging && python -m pip install -r requirements.txt && python3 -m pip freeze > requirements-current.txt && python3 runnotebook.py --endpoint storage.googleapis.com --bucket explorer-cloud-storage --directory 'Dag_generated_Explorera8daede3-e020-4df9-95ac-03b4890276d4' --dependencies-archive 'load_data-e326c245cc8e4e1d8f3b059f548a1d07.tar.gz' --file '/load_data.ipynb' --outputs 'data/noaa-weather-data-jfk-airport/jfk_weather.csv' "
+    ],
+    image="docker.io/amancevice/pandas:1.1.1",
+    env_vars={
+        "DATASET_URL": "https://dax-cdn.cdn.appdomain.cloud/dax-noaa-weather-data-jfk-airport/1.1.4/noaa-weather-data-jfk-airport.tar.gz",
+        "RUNTIME_ENV": "NotebookOp",
+        "AWS_ACCESS_KEY_ID": "GOOGL5U4Z4PN6BCIYVZRQPIZ",
+        "AWS_SECRET_ACCESS_KEY": "0PKhq1+tiY89IzXwwhF5MqXTYefFCfZl8mksiqe8",
+        "EXPLORER_RUN_NAME": "Dag_generated_Explorera8daede3-e020-4df9-95ac-03b4890276d4",
+    },
+    config_file=None,
+    image_pull_policy="Always",
+    dag=dag,
+)
+
+op_752e1e7205794567abceb6ea2965116f = KubernetesPodOperator(
+    task_id="752e1e7205794567abceb6ea2965116f",
+    name="752e1e7205794567abceb6ea2965116f",
+    namespace="default",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/krishnadhoundiyal/explorer/main/runnotebook.py --output runnotebook.py && curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/krishnadhoundiyal/explorer/main/requirements-explorer.txt --output requirements.txt && python3 -m pip install packaging && python -m pip install -r requirements.txt && python3 -m pip freeze > requirements-current.txt && python3 runnotebook.py --endpoint storage.googleapis.com --bucket explorer-cloud-storage --directory 'Dag_generated_Explorera8daede3-e020-4df9-95ac-03b4890276d4' --dependencies-archive 'Part 1 - Data Cleaning-752e1e7205794567abceb6ea2965116f.tar.gz' --file '/Part 1 - Data Cleaning.ipynb' --inputs 'data/noaa-weather-data-jfk-airport/jfk_weather.csv' --outputs 'data/noaa-weather-data-jfk-airport/jfk_weather_cleaned.csv' "
+    ],
+    image="docker.io/amancevice/pandas:1.1.1",
+    env_vars={
+        "d": "s",
+        "RUNTIME_ENV": "NotebookOp",
+        "AWS_ACCESS_KEY_ID": "GOOGL5U4Z4PN6BCIYVZRQPIZ",
+        "AWS_SECRET_ACCESS_KEY": "0PKhq1+tiY89IzXwwhF5MqXTYefFCfZl8mksiqe8",
+        "EXPLORER_RUN_NAME": "Dag_generated_Explorera8daede3-e020-4df9-95ac-03b4890276d4",
+    },
+    config_file=None,
+    image_pull_policy="Always",
+    dag=dag,
+)
+
+op_8da0e79319c14f4e9413d17ad5e3cd5d = KubernetesPodOperator(
+    task_id="8da0e79319c14f4e9413d17ad5e3cd5d",
+    name="8da0e79319c14f4e9413d17ad5e3cd5d",
+    namespace="default",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/krishnadhoundiyal/explorer/main/runnotebook.py --output runnotebook.py && curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/krishnadhoundiyal/explorer/main/requirements-explorer.txt --output requirements.txt && python3 -m pip install packaging && python -m pip install -r requirements.txt && python3 -m pip freeze > requirements-current.txt && python3 runnotebook.py --endpoint storage.googleapis.com --bucket explorer-cloud-storage --directory 'Dag_generated_Explorera8daede3-e020-4df9-95ac-03b4890276d4' --dependencies-archive 'Part 3 - Time Series Forecasting-8da0e79319c14f4e9413d17ad5e3cd5d.tar.gz' --file '/Part 3 - Time Series Forecasting.ipynb' --inputs 'data/noaa-weather-data-jfk-airport/jfk_weather_cleaned.csv;data/noaa-weather-data-jfk-airport/jfk_weather.csv' "
+    ],
+    image="docker.io/amancevice/pandas:1.1.1",
+    env_vars={
+        "d": "s",
+        "RUNTIME_ENV": "NotebookOp",
+        "AWS_ACCESS_KEY_ID": "GOOGL5U4Z4PN6BCIYVZRQPIZ",
+        "AWS_SECRET_ACCESS_KEY": "0PKhq1+tiY89IzXwwhF5MqXTYefFCfZl8mksiqe8",
+        "EXPLORER_RUN_NAME": "Dag_generated_Explorera8daede3-e020-4df9-95ac-03b4890276d4",
+    },
+    config_file=None,
+    image_pull_policy="Always",
+    dag=dag,
+)
+
+op_e326c245cc8e4e1d8f3b059f548a1d07 >> op_752e1e7205794567abceb6ea2965116f
+
+op_752e1e7205794567abceb6ea2965116f >> op_8da0e79319c14f4e9413d17ad5e3cd5d

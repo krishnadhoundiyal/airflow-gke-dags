57395306024c523e7434518ba615b2b4bd4b651c
krishnadhoundiyal <46423210+krishnadhoundiyal@users.noreply.github.com>
2021-11-11 19:37:41 +0530

    Pushed DAG Dag_generated_Explorer2bab4c3f-b9ed-483d-b41d-c4bf2a4a9f61

---
+++ Dag_generated_Explorer2bab4c3f-b9ed-483d-b41d-c4bf2a4a9f61.py
@@ -0,0 +1,81 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow import models
+import os
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+from airflow.kubernetes.secret import Secret
+
+
+args = {
+    "project_id": "Dag_generated_Explorer2bab4c3f-b9ed-483d-b41d-c4bf2a4a9f61",
+}
+
+dag = DAG(
+    "Dag_generated_Explorer2bab4c3f-b9ed-483d-b41d-c4bf2a4a9f61",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="Take This from User",
+    is_paused_upon_creation=False,
+)
+
+
+# Ensure that the secret named '0PKhq1+tiY89IzXwwhF5MqXTYefFCfZl8mksiqe8' is defined in the Kubernetes namespace where this pipeline will be run
+env_var_secret_id = Secret(
+    deploy_type="env",
+    deploy_target="AWS_ACCESS_KEY_ID",
+    secret="0PKhq1+tiY89IzXwwhF5MqXTYefFCfZl8mksiqe8",
+    key="AWS_ACCESS_KEY_ID",
+)
+env_var_secret_key = Secret(
+    deploy_type="env",
+    deploy_target="AWS_SECRET_ACCESS_KEY",
+    secret="0PKhq1+tiY89IzXwwhF5MqXTYefFCfZl8mksiqe8",
+    key="AWS_SECRET_ACCESS_KEY",
+)
+
+
+op_84991591a04b4678ab53add655c1a98a = KubernetesPodOperator(
+    task_id="84991591a04b4678ab53add655c1a98a",
+    name="84991591a04b4678ab53add655c1a98a",
+    namespace="default",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/krishnadhoundiyal/explorer/main/runnotebook.py --output runnotebook.py && curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/krishnadhoundiyal/explorer/main/requirements-explorer.txt --output requirements.txt && python3 -m pip install packaging && python -m pip install -r requirements.txt && python3 -m pip freeze > requirements-current.txt && python3 runnotebook.py --endpoint storage.googleapis.com --bucket explorer-cloud-storage --directory 'Dag_generated_Explorer2bab4c3f-b9ed-483d-b41d-c4bf2a4a9f61' --dependencies-archive 'load_data-84991591a04b4678ab53add655c1a98a.tar.gz' --file '/load_data.ipynb' --outputs 'data/noaa-weather-data-jfk-airport/jfk_weather.csv' "
+    ],
+    image="docker.io/amancevice/pandas:1.1.1",
+    env_vars={
+        "DATASET_URL": "https://dax-cdn.cdn.appdomain.cloud/dax-noaa-weather-data-jfk-airport/1.1.4/noaa-weather-data-jfk-airport.tar.gz",
+        "RUNTIME_ENV": "NotebookOp",
+        "AWS_ACCESS_KEY_ID": "GOOGL5U4Z4PN6BCIYVZRQPIZ",
+        "AWS_SECRET_ACCESS_KEY": "0PKhq1+tiY89IzXwwhF5MqXTYefFCfZl8mksiqe8",
+        "EXPLORER_RUN_NAME": "Dag_generated_Explorer2bab4c3f-b9ed-483d-b41d-c4bf2a4a9f61",
+    },
+    config_file=None,
+    image_pull_policy="Always",
+    dag=dag,
+)
+
+op_f576951e05d244bc8afd4d3db134e0bd = KubernetesPodOperator(
+    task_id="f576951e05d244bc8afd4d3db134e0bd",
+    name="f576951e05d244bc8afd4d3db134e0bd",
+    namespace="default",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/krishnadhoundiyal/explorer/main/runnotebook.py --output runnotebook.py && curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/krishnadhoundiyal/explorer/main/requirements-explorer.txt --output requirements.txt && python3 -m pip install packaging && python -m pip install -r requirements.txt && python3 -m pip freeze > requirements-current.txt && python3 runnotebook.py --endpoint storage.googleapis.com --bucket explorer-cloud-storage --directory 'Dag_generated_Explorer2bab4c3f-b9ed-483d-b41d-c4bf2a4a9f61' --dependencies-archive 'Part 1 - Data Cleaning-f576951e05d244bc8afd4d3db134e0bd.tar.gz' --file '/Part 1 - Data Cleaning.ipynb' --inputs 'data/noaa-weather-data-jfk-airport/jfk_weather.csv' --outputs 'data/noaa-weather-data-jfk-airport/jfk_weather_cleaned.csv' "
+    ],
+    image="docker.io/amancevice/pandas:1.1.1",
+    env_vars={
+        "d": "f",
+        "RUNTIME_ENV": "NotebookOp",
+        "AWS_ACCESS_KEY_ID": "GOOGL5U4Z4PN6BCIYVZRQPIZ",
+        "AWS_SECRET_ACCESS_KEY": "0PKhq1+tiY89IzXwwhF5MqXTYefFCfZl8mksiqe8",
+        "EXPLORER_RUN_NAME": "Dag_generated_Explorer2bab4c3f-b9ed-483d-b41d-c4bf2a4a9f61",
+    },
+    config_file=None,
+    image_pull_policy="Always",
+    dag=dag,
+)
+
+op_84991591a04b4678ab53add655c1a98a >> op_f576951e05d244bc8afd4d3db134e0bd
